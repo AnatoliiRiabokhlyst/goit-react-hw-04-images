fd0d1f1ddd93ad73324d91921398d8ec5972b29c
Anatolii <anatoliirst@gmail.com>
2022-07-06 14:34:53 +0200

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Searchbar from "./Searchbar";
+import ImageGallery from "./ImageGallery";
 
 
-export const App = () => {
-  return (
+class App extends React.Component{
+  state = {
+    search: '',
+}
+  onHandleSearch = search => {
+    this.setState({ search });
+}
+
+
+  render(){
+    return (
     <div>
-      <Searchbar/>
+      <Searchbar onSearch={this.onHandleSearch} />
+      <ImageGallery search={this.state.search} />
     </div>
   );
-};
+  }
+}
+
+
+export {App}
